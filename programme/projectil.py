fa2ede34a5cd9c96a7b90448e96599677908f2a1
E203870Y <dsd>
2022-01-04 12:23:32 +0100

    Grav + tire

--- programme/projectil.py
+++ programme/projectil.py
@@ -1,4 +1,6 @@
 import pygame
+import time
+import math
 from game_config import *
 from move import *
 
@@ -6,17 +8,41 @@ from move import *
 
 class projectil(pygame.sprite.Sprite):
     RIGHT = 1
-    def __init__(self,x,y,vx):
+    def __init__(self,x,y,velocity,angle):
         pygame.sprite.Sprite.__init__(self)
+        super().__init__()
+        
+        self.angle = math.radians( angle )
+        self.start_time = pygame.time.get_ticks()
         self.rect = pygame.Rect(x,y,GameConfig.PROJECTIL_W,GameConfig.PROJECTIL_H)
         self.sprite_count=0
         self.direction=projectil.RIGHT
         self.image =GameConfig.BALLE
         self.mask =GameConfig.BALLE_MASKS
-        self.vx = vx
+
+        self.vx = velocity * math.sin( self.angle )
+        self.vy = velocity * math.cos( self.angle )
+    
+
     def advance_state(self):
-        self.rect = self.rect.move(self.vx*GameConfig.DT, 0)
+        print(self.rect.y)
+        
+        self.rect.x+=self.vx
+        self.vy += GameConfig.GRAVITY*GameConfig.DT
+        self.rect.y +=self.vy
+        self.isdead()
+          
+            
+
     def draw(self, window):
+        self.temp= time.time()
         self.mask = GameConfig.BALLE_MASKS
         window.blit(self.image, self.rect)
+
+    def isdead(self):
+        if self.rect.y > 1000:
+            print("is dead fonctionne")
+            return True
+        else : return False
+        
         
